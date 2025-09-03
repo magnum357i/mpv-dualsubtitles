0b47a017773bd38f8f60928617ffac973d8ed781
bademli <berkankiran@hotmail.com>
2025-09-04 02:37:33 +0300

    some improvements

--- dualsubtitles.lua
+++ dualsubtitles.lua
@@ -9,10 +9,15 @@ options = {
 
 --Global Variables
 
-hideMode  = 0
-subtitles = {}
+hideMode       = 0
+subtitles      = {}
+prevTrackCount = 0
 
 --Helpers
+
+function log(str)
+print(str)
+end
 
 function splitString(str)
 local list = {}
@@ -37,8 +42,7 @@ end
 function getSubtitleList()
 local list              = {}
 local externalsubtitles = false
-local index, value
-for index, value in pairs(mp.get_property_native("track-list")) do
+for index, value in ipairs(mp.get_property_native("track-list")) do
     if value.type == "sub" then
         if value.external then
         local lang, bytes = detectSubtitleInfo(value["external-filename"])
@@ -49,18 +53,19 @@ for index, value in pairs(mp.get_property_native("track-list")) do
             value.metadata.NUMBER_OF_BYTES = bytes
             else
             value.lang                     = nil
-            print("Failed to get external subtitle info.")
+            log("Failed to get external subtitle info.")
             end
         end
     table.insert(list, value)
     end
+prevTrackCount = index
 end
 return list
 end
 
 function hasValue(items,value)
 local result = false
-for _, item in pairs(items) do
+for _, item in ipairs(items) do
 	if string.find(value, item) then
     result = true
 	break
@@ -85,7 +90,7 @@ local selectedSubtitles = {}
 local founded           = false
 local unwantedsubtitles = splitString(options.ignoredWords)
 for _, userLang in ipairs(languageCodes) do
-    for _, subtitle in pairs(subtitles) do
+    for _, subtitle in ipairs(subtitles) do
         if subtitle.lang and string.find(subtitle.lang, userLang) and filterSubtitle(subtitle, unwantedsubtitles) then
         founded = true
         table.insert(selectedSubtitles, subtitle)
@@ -113,18 +118,41 @@ end
 return 0
 end
 
---Functions
-
-function loadOrUpdate()
-subtitles = getSubtitleList()
+function formatTrackData(track)
+local dst = ""
+if track.title then
+dst = dst..string.format("'%s' ", track.title)
 end
+dst = dst.."("
+if track.lang then
+dst = dst..string.format("%s ", track.lang)
+end
+local codec = track.codec and track.codec or "<unknown>"
+codec = (codec == "subrip") and "subrip [Advanced Sub Station Alpha]" or codec
+dst = dst..codec
+dst = dst..")"
+local flags = {}
+if track["default"]          then table.insert(flags, "default")          end
+if track["forced"]           then table.insert(flags, "forced")           end
+if track["dependent"]        then table.insert(flags, "dependent")        end
+if track["visual-impaired"]  then table.insert(flags, "visual-impaired")  end
+if track["hearing-impaired"] then table.insert(flags, "hearing-impaired") end
+if track["external"]         then table.insert(flags, "external")         end
+if #flags > 0 then
+dst = dst.." ["..table.concat(flags, " ").."]"
+end
+dst = "("..track.id..") "..dst
+return dst
+end
+
+--Functions
 
 function setSubtitles()
 local bottomSid, topSid = findSubtitle(splitString(options.preferredLanguages.bottom)), findSubtitle(splitString(options.preferredLanguages.top))
 bottomSid               = (options.useTopAsBottom and bottomSid == 0 and topSid > 0) and topSid or bottomSid
 if bottomSid > 0 then mp.set_property_native("sid",           bottomSid) end
 if topSid > 0    then mp.set_property_native("secondary-sid", topSid)    end
-print(string.format("bottom %s, top %s", (bottomSid > 0) and bottomSid or "not set", (topSid > 0) and topSid or "not set"))
+log(string.format("bottom %s, top %s", (bottomSid > 0) and bottomSid or "not set", (topSid > 0) and topSid or "not set"))
 end
 
 function secondaryForward()
@@ -135,12 +163,7 @@ if topSid > #subtitles then
 mp.set_property_native("secondary-sid", 0)
 mp.osd_message("Secondary: no")
 else
-local subInfo = subtitles[topSid]
-    if subInfo.title then
-    mp.osd_message(string.format("Secondary: (%s) %s (\"%s\")", subInfo.id, subInfo.lang and subInfo.lang or "undefined", subInfo.title))
-    else
-    mp.osd_message(string.format("Secondary: (%s) %s",          subInfo.id, subInfo.lang and subInfo.lang or "undefined"))
-    end
+mp.osd_message("Secondary: "..formatTrackData(subtitles[topSid]))
 mp.set_property_native("secondary-sid", topSid)
 end
 end
@@ -153,12 +176,7 @@ if topSid == 0 then
 mp.set_property_native("secondary-sid", 0)
 mp.osd_message("Secondary: no")
 else
-local subInfo = subtitles[topSid]
-    if subInfo.title then
-    mp.osd_message(string.format("Secondary: (%s) %s (\"%s\")", subInfo.id, subInfo.lang and subInfo.lang or "undefined", subInfo.title))
-    else
-    mp.osd_message(string.format("Secondary: (%s) %s",          subInfo.id, subInfo.lang and subInfo.lang or "undefined"))
-    end
+mp.osd_message("Secondary: "..formatTrackData(subtitles[topSid]))
 mp.set_property_native("secondary-sid", topSid)
 end
 end
@@ -172,7 +190,7 @@ mp.set_property_native("sid",           topSid)
 mp.set_property_native("secondary-sid", bottomSid)
 local subInfo1 = subtitles[bottomSid]
 local subInfo2 = subtitles[topSid]
-mp.osd_message(string.format("Top Subtitle: (%s) %s\nBottom Subtitle: (%s) %s", subInfo1.id, subInfo1.lang and subInfo1.lang or "undefined", subInfo2.id, subInfo2.lang and subInfo2.lang or "undefined"))
+mp.osd_message(string.format("Top: %s\nBottom: %s", formatTrackData(subInfo1), formatTrackData(subInfo2)))
 else
 mp.osd_message("Subtitles not reversed")
 end
@@ -197,8 +215,12 @@ mp.osd_message("Subtitles visible")
 end
 end
 
-mp.observe_property("track-list", "native", function(name,value)
-    loadOrUpdate()
+mp.observe_property("track-list", "native", function(event,tracklist)
+    if #tracklist ~= prevTrackCount then
+    local firstUpdated = (prevTrackCount == 0) and true or false
+    subtitles          = getSubtitleList()
+    if not firstUpdated then log("Subtitle list updated.") end
+    end
 end)
 
 mp.add_key_binding("k", "secondaryforward",  secondaryForward)
